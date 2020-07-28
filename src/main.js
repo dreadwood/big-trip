1422dbd5c3de1c0c2e8f92f29e5dd52f9fbedd8b
srg <skabass@yandex.ru>
2020-07-29 01:48:33 +0300

    1.5 Отрисовывает элементы на странице

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const EVENT_COUNT = 3;
+
 const RenderPosition = {
   AFTER_BEGIN: `afterbegin`,
   BEFOR_END: `beforeend`,
@@ -400,3 +402,24 @@ const createStatisticTemplate = () => {
 const render = (container, template, place = RenderPosition.BEFOR_END) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const bodyElement = document.querySelector(`.page-body`);
+const headerMainElement = bodyElement.querySelector(`.trip-main`);
+const headerControlsElement = headerMainElement.querySelector(`.trip-controls`);
+const pageContainerElement = bodyElement.querySelector(`.trip-events`);
+
+render(headerMainElement, createTripInfoTemplate(), RenderPosition.AFTER_BEGIN);
+render(headerControlsElement, createMenuTemplate(), RenderPosition.AFTER_BEGIN);
+render(headerControlsElement, createFiltersTemplate());
+
+render(pageContainerElement, createSortingTemplate());
+render(pageContainerElement, createEventEditTemplate());
+render(pageContainerElement, createDayListTemplate());
+
+const dayListElement = pageContainerElement.querySelector(`.trip-days`);
+render(dayListElement, createDayTemplate());
+
+const eventListElement = dayListElement.querySelector(`.trip-events__list`);
+for (let i = 0; i < EVENT_COUNT; i++) {
+  render(eventListElement, createEventTemplate());
+}
