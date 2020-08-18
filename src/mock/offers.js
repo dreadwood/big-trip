4eafa744e8058b021ef39f9e3d2391b37edafaa7
srg <skabass@yandex.ru>
2020-08-18 12:48:56 +0300

    3.5 Создает отдельную структуру для дополнительных опций

---
+++ src/mock/offers.js
@@ -0,0 +1,87 @@
+export const TYPES_OF_OFFERS = [
+  {
+    type: `flight`,
+    offers: [
+      {
+        type: `luggage`,
+        description: `Add luggage`,
+        price: 30,
+      },
+      {
+        type: `comfort`,
+        description: `Switch to comfort class`,
+        price: 100,
+      },
+      {
+        type: `meal`,
+        description: `Add meal`,
+        price: 15,
+      },
+      {
+        type: `seats`,
+        description: `Choose seats`,
+        price: 5,
+      },
+      {
+        type: `train`,
+        description: `Travel by train`,
+        price: 40,
+      },
+    ]
+  },
+  {
+    type: `check-in`,
+    offers: [
+      {
+        type: `breakfast`,
+        description: `Add breakfast`,
+        price: 50,
+      },
+      {
+        type: `ironing`,
+        description: `Ironing service`,
+        price: 15,
+      },
+      {
+        type: `spa`,
+        description: `Spa`,
+        price: 80,
+      },
+    ],
+  },
+  {
+    type: `taxi`,
+    offers: [
+      {
+        type: `uber`,
+        description: `Order Uber`,
+        price: 20,
+      },
+    ],
+  },
+  {
+    type: `drive`,
+    offers: [
+      {
+        type: `rent`,
+        description: `Rent a car`,
+        price: 200,
+      },
+    ],
+  },
+  {
+    type: `sightseeing`,
+    offers: [
+      {
+        type: `tickets`,
+        description: `Book tickets`,
+        price: 40,
+      },
+      {
+        type: `lunch`,
+        description: `Lunch in city`,
+        price: 30,
+      },
+    ],
+  },
+];
