d0ae8bd480fbfadab0660650987d70434f9db592
srg <skabass@yandex.ru>
2020-08-19 00:45:01 +0300

    3.11 Шаблонизирует фильтры, меню и сообщения страницы

--- src/view/filters.js
+++ src/view/filters.js
@@ -1,39 +1,28 @@
-export const createFiltersTemplate = () => {
+import {capitalizeStr} from '../utils.js';
+
+const filters = [
+  `everything`,
+  `future`,
+  `past`,
+];
+
+export const createFiltersTemplate = (selectedFilter = `everything`) => {
   return (
     `<form class="trip-filters" action="#" method="get">
-      <div class="trip-filters__filter">
+      ${filters.map((filter) => `<div class="trip-filters__filter">
         <input
-          id="filter-everything"
-          class="trip-filters__filter-input  visually-hidden"
+          id="filter-${filter}"
+          class="trip-filters__filter-input visually-hidden"
           type="radio"
           name="trip-filter"
-          value="everything"
-          checked
+          value="${filter}"
+          ${filter === selectedFilter ? `checked` : ``}
         >
-        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input
-          id="filter-future"
-          class="trip-filters__filter-input  visually-hidden"
-          type="radio"
-          name="trip-filter"
-          value="future"
-        >
-        <label class="trip-filters__filter-label" for="filter-future">Future</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input
-          id="filter-past"
-          class="trip-filters__filter-input  visually-hidden"
-          type="radio"
-          name="trip-filter"
-          value="past"
-        >
-        <label class="trip-filters__filter-label" for="filter-past">Past</label>
-      </div>
+        <label
+          class="trip-filters__filter-label"
+          for="filter-${filter}"
+        >${capitalizeStr(filter)}</label>
+      </div>`).join(`\n`)}
 
       <button class="visually-hidden" type="submit">Accept filter</button>
     </form>`
--- src/view/menu.js
+++ src/view/menu.js
@@ -1,8 +1,15 @@
-export const createMenuTemplate = () => {
+const tabs = [
+  `Table`,
+  `Stats`
+];
+
+export const createMenuTemplate = (selectedFilter = `Table`) => {
   return (
     `<nav class="trip-controls__trip-tabs  trip-tabs">
-      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-      <a class="trip-tabs__btn" href="#">Stats</a>
+      ${tabs.map((item) => `<a
+        class="trip-tabs__btn ${item === selectedFilter ? `trip-tabs__btn--active` : ``}"
+        href="#"
+      >${item}</a>`).join(`\n`)}
     </nav>`
   );
 };
