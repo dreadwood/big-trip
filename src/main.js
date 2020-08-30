d6de6d64755cc20cc2f77b46f7629afe689e2c4c
srg <skabass@yandex.ru>
2020-08-30 17:37:57 +0300

    5.1.6 Переносит логику отрисовки событий в презентер

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import TripInfoView from './view/trip-info.js';
 import MenuView from './view/menu.js';
 import FiltersView from './view/filters.js';
-import SortingView from './view/sorting.js';
-import DayListView from './view/day-list.js';
-import DayView from './view/day.js';
-import EventView from './view/event.js';
-import EventEditView from './view/event-edit.js';
-import PageMessageView from './view/page-message.js';
 // import StatisticView from './view/statistic.js';
+import TripPresenter from './presenter/trip.js';
 import {generateTripEvent} from './mock/trip-event.js';
-import {RenderPosition, render, replace} from './utils/render.js';
+import {RenderPosition, render} from './utils/render.js';
 
 const EVENT_COUNT = 20;
 
@@ -17,71 +12,15 @@ const events = new Array(EVENT_COUNT).fill(``)
   .map(generateTripEvent)
   .sort((a, b) => a.startDate - b.startDate);
 
-const datesEvents = [...new Set(events
-  .map((event) => event.startDate.toDateString()))];
-
-const renderEvent = (container, event) => {
-  const eventComponent = new EventView(event);
-  const eventEditComponent = new EventEditView(event);
-
-  const replaceCardToForm = () => {
-    replace(eventEditComponent, eventComponent);
-  };
-
-  const replaceFormToCard = () => {
-    replace(eventComponent, eventEditComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === `Escape` || evt.key === `Esc`) {
-      evt.preventDefault();
-      replaceFormToCard();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  eventComponent.setArrowButtonClickHandler(() => {
-    replaceCardToForm();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  eventEditComponent.setFormSubmitHandler(() => {
-    replaceFormToCard();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  eventEditComponent.setArrowButtonClickHandler(() => {
-    replaceFormToCard();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  render(container, eventComponent);
-};
-
 const bodyElement = document.querySelector(`.page-body`);
 const headerMainElement = bodyElement.querySelector(`.trip-main`);
 const headerControlsElement = headerMainElement.querySelector(`.trip-controls`);
-const pageContainerElement = bodyElement.querySelector(`.trip-events`);
+const pageContainerElement = bodyElement.querySelector(`.trip-events`); // this._tripContainer
 
-render(headerMainElement, new TripInfoView(events), RenderPosition.AFTER_BEGIN);
-render(headerControlsElement, new MenuView(), RenderPosition.AFTER_BEGIN);
-render(headerControlsElement, new FiltersView());
+const tripPresenter = new TripPresenter(pageContainerElement);
 
-if (events.length === 0) {
-  render(pageContainerElement, new PageMessageView());
-} else {
-  render(pageContainerElement, new SortingView());
-  const dayListComponent = new DayListView();
-  render(pageContainerElement, dayListComponent);
+render(headerMainElement, new TripInfoView(events), RenderPosition.AFTER_BEGIN); // информация о поездке
+render(headerControlsElement, new MenuView(), RenderPosition.AFTER_BEGIN); // меню
+render(headerControlsElement, new FiltersView()); // фильтрация
 
-  datesEvents.forEach((date, i) => {
-    const dayComponent = new DayView(date, i);
-    const eventListElement = dayComponent.getElement().querySelector(`.trip-events__list`);
-
-    events
-      .filter((event) => event.startDate.toDateString() === date)
-      .forEach((event) => renderEvent(eventListElement, event));
-
-    render(dayListComponent, dayComponent);
-  });
-}
+tripPresenter.init(events);
--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -4,19 +4,26 @@ import DayView from '../view/day.js';
 import EventView from '../view/event.js';
 import EventEditView from '../view/event-edit.js';
 import PageMessageView from '../view/page-message.js';
-import {RenderPosition, render, replace} from './utils/render.js';
+import {render, replace} from '../utils/render.js';
 
-export default class Trip {
+export default class TripPresenter {
   constructor(tripContainer) {
     this._tripContainer = tripContainer;
 
     this._sortingComponent = new SortingView();
     this._dayListComponent = new DayListView();
-    this._pageMessageComponent = new PageMessageView();
   }
 
-  init() {
+  init(events) {
     // инициализация
+    this._events = events;
+
+    if (this._events.length === 0) {
+      this._renderNoEvents();
+    } else {
+      this._renderSorting();
+      this._renderDayList();
+    }
   }
 
   _sortEvents() {
@@ -29,28 +36,79 @@ export default class Trip {
 
   _renderSorting() {
     // рендер панели сортировки
+    render(this._tripContainer, new SortingView());
     // установка обработчика сортировки
-  }
-
-  _renderDay() {
-    // рендер одного дня
-  }
-
-  // разделить на дни / редарить несколько дней
-
-  _renderDayList() {
-    // рендер списка дней
-  }
-
-  _renderEvent(event) {
-    // рендер события
   }
 
   _renderNewEvent() {
     // рендер нового события
   }
 
+  _renderEvent(dayСontainer, event) {
+    // рендер события
+    const eventComponent = new EventView(event);
+    const eventEditComponent = new EventEditView(event);
+
+    const replaceCardToForm = () => {
+      replace(eventEditComponent, eventComponent);
+    };
+
+    const replaceFormToCard = () => {
+      replace(eventComponent, eventEditComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    eventComponent.setArrowButtonClickHandler(() => {
+      replaceCardToForm();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+    eventEditComponent.setFormSubmitHandler(() => {
+      replaceFormToCard();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    eventEditComponent.setArrowButtonClickHandler(() => {
+      replaceFormToCard();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    render(dayСontainer, eventComponent);
+  }
+
+  _renderDay(date, i) {
+    // рендер одного дня
+    const dayComponent = new DayView(date, i);
+    const dayСontainer = dayComponent.getElement().querySelector(`.trip-events__list`);
+
+    this._events
+      .filter((event) => event.startDate.toDateString() === date)
+      .forEach((event) => this._renderEvent(dayСontainer, event));
+
+    render(this._dayListComponent, dayComponent);
+  }
+
+  // разделить на дни / рендарить несколько дней
+
+  _renderDayList() {
+    // рендер списка дней
+    render(this._tripContainer, this._dayListComponent);
+
+    const datesEvents = [...new Set(this._events
+      .map((event) => event.startDate.toDateString()))];
+
+    datesEvents.forEach((date, i) => this._renderDay(date, i));
+  }
+
   _renderNoEvents() {
     // рендер сообщение о отсутствии событий
+    render(this._tripContainer, new PageMessageView());
   }
 }
