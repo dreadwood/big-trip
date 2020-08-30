0b57a8bd97ec3e69122aeea5bfe5aaffcdaed1cb
srg <skabass@yandex.ru>
2020-08-30 20:13:10 +0300

    5.2.2 Рефакторинг компонента сортировки

--- src/view/sorting.js
+++ src/view/sorting.js
@@ -1,34 +1,42 @@
 import {capitalizeStr} from '../utils/common.js';
 import AbstractView from "./abstract.js";
 
-const SORTING_TYPES = [
-  `event`,
-  `time`,
-  `price`,
-];
+const SortingTypes = {
+  EVENT: `sort-event`,
+  TIME: `sort-time`,
+  PRICE: `sort-price`,
+};
+
+const createInputTemplate = (selectedSort) => Object.entries(SortingTypes).map(([type, value]) => {
+  const typeName = type.toLowerCase();
+
+  return (
+    `<div class="trip-sort__item trip-sort__item--${typeName}">
+        <input
+          id="${value}"
+          class="trip-sort__input  visually-hidden"
+          type="radio"
+          name="trip-sort"
+          value="${value}"
+          ${value === selectedSort ? `checked` : ``}
+        >
+        <label class="trip-sort__btn" for="sort-${typeName}">
+          ${capitalizeStr(typeName)}
+          ${value !== selectedSort ? `<svg
+            class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+              <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+            </svg>` : ``}
+        </label>
+      </div>`
+  );
+});
 
 const createSortingTemplate = (selectedSort) => {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <span class="trip-sort__item  trip-sort__item--day">Day</span>
 
-      ${SORTING_TYPES.map((item) =>`<div class="trip-sort__item trip-sort__item--${item}">
-        <input
-          id="sort-${item}"
-          class="trip-sort__input  visually-hidden"
-          type="radio"
-          name="trip-sort"
-          value="sort-${item}"
-          ${item === selectedSort ? `checked` : ``}
-        >
-        <label class="trip-sort__btn" for="sort-${item}">
-          ${capitalizeStr(item)}
-          ${item !== selectedSort ? `<svg
-            class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-              <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-            </svg>` : ``}
-        </label>
-      </div>`).join(`\n`)}
+      ${createInputTemplate(selectedSort).join(`\n`)}
 
       <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
     </form>`
@@ -36,7 +44,7 @@ const createSortingTemplate = (selectedSort) => {
 };
 
 export default class SortingView extends AbstractView {
-  constructor(selectedSort = `event`) {
+  constructor(selectedSort = SortingTypes.EVENT) {
     super();
 
     this._selectedSort = selectedSort;
