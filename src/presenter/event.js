d05ac47b65c3341feecd7bce9bd276f23bcc241b
srg <skabass@yandex.ru>
2020-09-04 22:32:03 +0300

    6.1.3 Добавляет возможность переиспользования

--- src/presenter/event.js
+++ src/presenter/event.js
@@ -1,6 +1,6 @@
 import EventView from '../view/event.js';
 import EventEditView from '../view/event-edit.js';
-import {render, replace} from '../utils/render.js';
+import {render, replace, remove} from '../utils/render.js';
 
 export default class EventPresenter {
   constructor(dayСontainer) {
@@ -18,16 +18,36 @@ export default class EventPresenter {
   init(event) {
     this._event = event;
 
+    const prevEventComponent = this._eventComponent;
+    const prevEventEditComponent = this._eventEditComponent;
+
     this._eventComponent = new EventView(event);
     this._eventEditComponent = new EventEditView(event);
 
     this._eventComponent.setArrowButtonClickHandler(this._handleCardArrowButtonClick);
-
     this._eventEditComponent.setFormSubmitHandler(this._handleFormSubmit);
-
     this._eventEditComponent.setArrowButtonClickHandler(this._handleFormArrowButtonClick);
 
-    render(this._dayСontainer, this._eventComponent);
+    if (prevEventComponent === null || prevEventEditComponent === null) {
+      render(this._dayСontainer, this._eventComponent);
+      return;
+    }
+
+    if (this._dayСontainer.getElement().contains(prevEventComponent.getElement())) {
+      replace(this._eventComponent, prevEventComponent);
+    }
+
+    if (this._dayСontainer.getElement().contains(prevEventEditComponent.getElement())) {
+      replace(this._eventEditComponent, prevEventEditComponent);
+    }
+
+    remove(prevEventComponent);
+    remove(prevEventEditComponent);
+  }
+
+  destroy() {
+    remove(this._eventComponent);
+    remove(this._eventEditComponent);
   }
 
   _replaceCardToForm() {
