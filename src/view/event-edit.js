a4d6f7944a19d543c5820a462a1ef09bd53335ea
srg <skabass@yandex.ru>
2020-09-07 00:11:13 +0300

    6.1.8 Добавляет метод для обновления шаблона компонента

--- src/view/event-edit.js
+++ src/view/event-edit.js
@@ -274,6 +274,17 @@ export default class EventEditView extends AbstractView {
     return createEventEditTemplate(this._data);
   }
 
+  updateElement() {
+    let prevElement = this.getElement();
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parent.replaceChild(newElement, prevElement);
+    prevElement = null;
+  }
+
   _formSubmitHandler(evt) {
     evt.preventDefault();
     this._callback.formSubmit(EventEditView.parseDataToEvent(this._data));
