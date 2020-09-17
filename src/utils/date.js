3275842c8750e84d2730d6a98480947f349c6b3a
srg <skabass@yandex.ru>
2020-09-17 22:29:33 +0300

    6.2.4 Настраивает moment для длительности события

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 export const getTime = (date) => { // 19:26
-  return moment(date).format(`HH:MM`);
+  return moment(date).format(`HH:mm`);
 };
 
 export const getDatetime = (date) => { // 2020-08-19
@@ -9,11 +9,11 @@ export const getDatetime = (date) => { // 2020-08-19
 };
 
 export const getFullDatetime = (date) => { // 2020-08-19T12:08
-  return moment(date).format(`YYYY-MM-DD[T]HH:MM`);
+  return moment(date).format(`YYYY-MM-DD[T]HH:mm`);
 };
 
 export const getFullDateWithSlash = (date) => { // 19/08/20 12:08
-  return moment(date).format(`DD/MM/YY HH:MM`);
+  return moment(date).format(`DD/MM/YY HH:mm`);
 };
 
 export const getShortDate = (date) => { // 19 AUG
@@ -24,12 +24,16 @@ export const getShortDateInversion = (date) => { // AUG 19
   return moment(date).format(`MMM DD`);
 };
 
-export const getFormatDuration = (durationInMs) => { // 1H 25M
-  const durationInMin = durationInMs / (60 * 1000);
+export const getFormatDuration = (durationInMs) => { // 1D 1H 25M
+  const minutes = moment.duration(durationInMs, `ms`).minutes();
+  const hours = moment.duration(durationInMs, `ms`).hours();
+  const days = moment.duration(durationInMs, `ms`).days();
 
-  return (durationInMin > 60)
-    ? `${Math.floor(durationInMin / 60)}H ${durationInMin % 60}M`
-    : `${durationInMin}M`;
+  const d = days ? `${days}D` : ``;
+  const h = hours ? `${hours}H` : ``;
+  const m = minutes ? `${minutes}M` : ``;
+
+  return `${d} ${h} ${m}`;
 };
 
 export const getDurationEvent = (event) => event.endDate - event.startDate; // ms (number)
