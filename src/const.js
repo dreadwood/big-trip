6cdeb92f61739b59d94f11281392af10676f0bd3
srg <skabass@yandex.ru>
2020-09-29 22:30:54 +0300

    7.1.8 Добавляет модель для фильтра

--- src/const.js
+++ src/const.js
@@ -15,3 +15,9 @@ export const UpdateType = {
   MINOR: `MINOR`,
   MAJOR: `MAJOR`
 };
+
+export const FilterType = {
+  EVERYTHING: `Everything`,
+  FUTURE: `Future`,
+  PAST: `Past`,
+};
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import TripInfoView from './view/trip-info.js';
 import MenuView from './view/menu.js';
 import FiltersView from './view/filters.js';
 import EventsModel from './model/events.js';
+import FilterModel from './model/filter.js';
 // import StatisticView from './view/statistic.js';
 import TripPresenter from './presenter/trip.js';
 import {generateTripEvent} from './mock/trip-event.js';
@@ -10,11 +11,12 @@ import {RenderPosition, render} from './utils/render.js';
 const EVENT_COUNT = 20;
 
 const events = new Array(EVENT_COUNT).fill(``)
-  .map(generateTripEvent)
-  .sort((a, b) => a.startDate - b.startDate);
+  .map(generateTripEvent);
 
 const eventsModel = new EventsModel();
 eventsModel.setEvents(events);
+
+const filterModel = new FilterModel();
 
 const bodyElement = document.querySelector(`.page-body`);
 const headerMainElement = bodyElement.querySelector(`.trip-main`);
